70c31e2e5e58003c4caf66405dc66d7d9ff8020e
TaylanCann <taylancanh@gmail.com>
2022-03-31 20:55:15 +0300

    Singleton Example

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+
+        // This is the static method that controls the access to the singleton
+        // instance. On the first run, it creates a singleton object and places
+        // it into the static field. On subsequent runs, it returns the client
+        // existing object stored in the static field.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // Finally, any singleton should define some business logic, which can
+        // be executed on its instance.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+}
